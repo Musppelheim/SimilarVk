0de916aa0fb7f4040acbf0107f8216db4512e456
Musppelheim <neloliver@hotmail.com>
2020-03-28 21:33:43 +0700

    Manager added

---
+++ src/main/java/manager/PostManager.java
@@ -0,0 +1,17 @@
+package manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+    Post post = new Post();
+
+    public Post[] search(int ownerId) {
+        //to do
+        return null;
+    }
+
+    public void delete(int ownerId, int postId) {
+    }
+
+}
