42b51d9f8f91e7f4fe223563c89d1977d0018c64
Musppelheim <neloliver@hotmail.com>
2020-03-28 16:32:39 +0700

    Initial commit #v1

---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,10 @@
+package ru.netology.domain;
+
+public class Post {
+    private String id;
+    private String imageUrl;
+    private String like;
+    private String comment;
+    private String share;
+    private String view;
+}
